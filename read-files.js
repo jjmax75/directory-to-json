59553f06f75bb24d6dc0b1bca2c45a911fb7971e
John Behan <behanj@gmail.com>
2018-07-22 17:04:42 +0100

    first commit

---
+++ read-files.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+var path = require('path');
+var dirTree = '/Users/john/Downloads/pythonforkids';
+
+function logger(err, res) {
+  if (err) console.log(err);
+
+  console.log(JSON.stringify(res));
+}
+
+var directoryTreeToObj = function(dir, done) {
+    var results = [];
+
+    fs.readdir(dir, function(err, list) {
+        if (err)
+            return done(err);
+
+        var pending = list.length;
+
+        if (!pending)
+            return done(null, {name: path.basename(dir), type: 'folder', children: results});
+
+        list.forEach(function(file) {
+            file = path.resolve(dir, file);
+            fs.stat(file, function(err, stat) {
+                if (stat && stat.isDirectory()) {
+                    directoryTreeToObj(file, function(err, res) {
+                        results.push({
+                            name: path.basename(file),
+                            type: 'folder',
+                            children: res
+                        });
+                        if (!--pending)
+                            done(null, results);
+                    });
+                }
+                else {
+                    results.push({
+                        type: 'file',
+                        name: path.basename(file)
+                    });
+                    if (!--pending)
+                        done(null, results);
+                }
+            });
+        });
+    });
+};
+
+directoryTreeToObj(dirTree, logger)
